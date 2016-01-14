f886cbe72ae9a697372eccbd083225a62030b8e3
Ryan Kraus <rmkraus@gmail.com>
2016-01-13 19:24:04 -0500

    Removed extra function from setup.py
    Removed the install_scripts_and_symlinks function from setup.py. This
    appears to be attempting to link executable scripts to somewhere or
    some nonsense. Whatever. It totally breaks on *nix systems without
    elevated privileges and it isn’t even the correct way to do what it is
    trying to do.
    
    Also, the package still seems to work with it removed. So… Gonzo.

--- setup.py
+++ setup.py
@@ -4,27 +4,7 @@
 import os.path
 from setuptools import setup, find_packages
 from distutils.command.install_scripts import install_scripts
-
 from distutils.core import setup
-
-class install_scripts_and_symlinks(install_scripts):
-    '''Like install_scripts, but also replicating nonexistent symlinks'''
-    def run(self):
-      print ("=============install_scripts_and_symlinks run")
-      install_scripts.run(self)
-      # Replicate symlinks if they don't exist
-      print (self)
-      print ("data_files = ",  dir( self.distribution.data_files))
-      print (type(self.distribution.data_files))
-      print (self.distribution.data_files)
-      for script in self.distribution.scripts:
-        print  ("\n---script = ",script)
-        if os.path.islink(script):
-          target  = os.readlink(script)
-          newlink = os.path.join(self.install_dir, os.path.basename(script))
-          if not os.path.exists(newlink):
-            print ("++++++++++", target, " -> ", newlink)
-            os.symlink(target, newlink)
 
 
 setup(
@@ -38,6 +18,5 @@ setup(
     url='https://github.com/philippelt/netatmo-api-python',
     license='Open Source',
     description='Simple API to access Netatmo weather station data from any python script.',
-    long_description=open('README.md').read(),
-    cmdclass = { 'install_scripts': install_scripts_and_symlinks }
+    long_description=open('README.md').read()
 )
