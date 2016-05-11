bdf2929716e84467145f04cdcfe272941ab6d9e0
Hugo D. (jabesq) <jabesq@gmail.com>
2016-05-11 18:15:52 +0200

    [WELCOME] Type detection for image retrieved
    Image returned by the Welcome API can be either png (profile image) or Jpeg
    (camera snapshot)
    
    Signed-off-by: Hugo D. (jabesq) <jabesq@gmail.com>

--- lnetatmo.py
+++ lnetatmo.py
@@ -12,6 +12,7 @@
 
 from sys import version_info
 import json, time
+import imghdr
 
 # HTTP libraries depends upon Python 2 or 3
 if version_info.major == 3 :
@@ -230,60 +231,6 @@ class DeviceList:
         else:
             return None
 
-# Utilities routines
-
-def postRequest(url, params, json_resp=True, body_size=65535):
-    # Netatmo response body size limited to 64k (should be under 16k)
-    if version_info.major == 3:
-        req = urllib.request.Request(url)
-        req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
-        params = urllib.parse.urlencode(params).encode('utf-8')
-        resp = urllib.request.urlopen(req, params).read(body_size).decode("utf-8")
-    else:
-        params = urlencode(params)
-        headers = {"Content-Type" : "application/x-www-form-urlencoded;charset=utf-8"}
-        req = urllib2.Request(url=url, data=params, headers=headers)
-        resp = urllib2.urlopen(req).read(body_size)
-    if json_resp:
-        return json.loads(resp)
-    else:
-        return resp
-
-def toTimeString(value):
-    return time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(int(value)))
-
-def toEpoch(value):
-    return int(time.mktime(time.strptime(value,"%Y-%m-%d_%H:%M:%S")))
-
-def todayStamps():
-    today = time.strftime("%Y-%m-%d")
-    today = int(time.mktime(time.strptime(today,"%Y-%m-%d")))
-    return today, today+3600*24
-
-# Global shortcut
-
-def getStationMinMaxTH(station=None, module=None):
-    authorization = ClientAuth()
-    devList = DeviceList(authorization)
-    if not station : station = devList.default_station
-    if module :
-        mname = module
-    else :
-        mname = devList.stationByName(station)['module_name']
-    lastD = devList.lastData(station)
-    if mname == "*":
-        result = dict()
-        for m in lastD.keys():
-            if time.time()-lastD[m]['When'] > 3600 : continue
-            r = devList.MinMaxTH(module=m)
-            result[m] = (r[0], lastD[m]['Temperature'], r[1])
-    else:
-        if time.time()-lastD[mname]['When'] > 3600 : result = ["-", "-"]
-        else : result = [lastD[mname]['Temperature'], lastD[mname]['Humidity']]
-        result.extend(devList.MinMaxTH(station, mname))
-    return result
-
-
 class WelcomeData:
 
     def __init__(self, authData):
@@ -361,11 +308,66 @@ class WelcomeData:
             "key" : key
             }
         resp = postRequest(_GETCAMERAPICTURE_REQ, postParams, json_resp=False, body_size=131070)
-        return resp
+        image_type = imghdr.what('NONE.FILE',resp)
+        return resp, image_type
 
     def someoneKnownSeen(self):
         #Check in the last event is someone known has been seen
         pass
+
+# Utilities routines
+
+def postRequest(url, params, json_resp=True, body_size=65535):
+    # Netatmo response body size limited to 64k (should be under 16k)
+    if version_info.major == 3:
+        req = urllib.request.Request(url)
+        req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
+        params = urllib.parse.urlencode(params).encode('utf-8')
+        resp = urllib.request.urlopen(req, params).read(body_size).decode("utf-8")
+    else:
+        params = urlencode(params)
+        headers = {"Content-Type" : "application/x-www-form-urlencoded;charset=utf-8"}
+        req = urllib2.Request(url=url, data=params, headers=headers)
+        resp = urllib2.urlopen(req).read(body_size)
+
+    if json_resp:
+        return json.loads(resp)
+    else:
+        return resp
+
+def toTimeString(value):
+    return time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(int(value)))
+
+def toEpoch(value):
+    return int(time.mktime(time.strptime(value,"%Y-%m-%d_%H:%M:%S")))
+
+def todayStamps():
+    today = time.strftime("%Y-%m-%d")
+    today = int(time.mktime(time.strptime(today,"%Y-%m-%d")))
+    return today, today+3600*24
+
+# Global shortcut
+
+def getStationMinMaxTH(station=None, module=None):
+    authorization = ClientAuth()
+    devList = DeviceList(authorization)
+    if not station : station = devList.default_station
+    if module :
+        mname = module
+    else :
+        mname = devList.stationByName(station)['module_name']
+    lastD = devList.lastData(station)
+    if mname == "*":
+        result = dict()
+        for m in lastD.keys():
+            if time.time()-lastD[m]['When'] > 3600 : continue
+            r = devList.MinMaxTH(module=m)
+            result[m] = (r[0], lastD[m]['Temperature'], r[1])
+    else:
+        if time.time()-lastD[mname]['When'] > 3600 : result = ["-", "-"]
+        else : result = [lastD[mname]['Temperature'], lastD[mname]['Humidity']]
+        result.extend(devList.MinMaxTH(station, mname))
+    return result
 
 # auto-test when executed directly
 
