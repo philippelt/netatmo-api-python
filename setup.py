c6be4fe356c5bf7f9710ad1605086fcbffd12672
GP8x <GP8x@users.noreply.github.com>
2018-09-07 23:32:38 +0100

    v1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setup(
     name='pyatmo',
-    version='1.1.1',  # Should be updated with new versions
+    version='1.2',  # Should be updated with new versions
     author='Hugo Dupras',
     author_email='jabesq@gmail.com',
     py_modules=['pyatmo'],
