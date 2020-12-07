41448e9ff54c2576806ceab72281f0f21c42ded8
Philippe Larduinat <ph.larduinat@biz-planet.eu>
2020-12-07 15:50:08 +0100

    [Minor] Prepare new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='lnetatmo',
-    version='2.0.0',
+    version='2.1.0',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -17,7 +17,7 @@ setup(
     scripts=[],
     data_files=[],
     url='https://github.com/philippelt/netatmo-api-python',
-    download_url='https://github.com/philippelt/netatmo-api-python/tarball/v2.0.0.tar.gz',
+    download_url='https://github.com/philippelt/netatmo-api-python/tarball/v2.1.0.tar.gz',
     license='GPL V3',
     description='Simple API to access Netatmo weather station data from any python script.'
 )
